6b149244c47c2de6d92ff748d02f1ceb15cdbde5
Lin <lzh130@uclive.ac.nz>
2013-04-25 14:07:52 +1200

    update and get work using thin

--- main.rb
+++ main.rb
@@ -1,40 +1,55 @@
 require 'sinatra/base'
+require 'dm-core'
+require 'dm-migrations'
+require 'dm-serializer/to_json'
 require 'json'
 require 'haml'
 require 'pry'
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, 'postgres://Lin@localhost/maklog')
 
+class Posts
+	include DataMapper::Resource
+	property :id, Serial
+	property :title, String
+	property :author, String
+	property :intro, Text
+	property :publishedAt, Date
+	property :description, Text
+end
+DataMapper.finalize
+#DataMapper.auto_migrate!
 class MyApp < Sinatra::Base
 	set :public_folder, File.dirname(__FILE__) + '/static'
 
 	before do
 		content_type 'application/json'
 	end
+	get '/test' do
+		binding.pry
+	end
 
 	get '/posts' do
-		hash = {
-			posts: [
-				{
-					id:1,
-					author:'Lin',
-					intro:"Today really a *good day*, because you are around",
-					publishedAt: Date.new(1985,4,14),
-					description:<<-EOF 
-It should be **the day** before, I was still jerking off in my car, in the middle noon Thursday. There is a old lady who trying to park her car in the block next to me, when I was about to move my luggage in the front row. I thought she is too old to notice the unusual activity was going on, but unfortunitily she does. so they maybe report a suspected car was parking outside the community parking lot and propbly sleeping there for the night.
-EOF
-				},
-				{
-					id:2,
-					author:'Sheepsheep',
-					intro:"Today I'm not happy, because you are not near",
-					publishedAt: Date.new(1984,10,1),
-					description:<<-EOF
-It should be the day before, I was still jerking off in my car, in the middle noon Thursday. There is a old lady who trying to park her car in the block next to me, when I was about to move my luggage in the front row. I thought she is too old to notice the unusual activity was going on, but unfortunitily she does. so they maybe report a suspected car was parking outside the community parking lot and propbly sleeping there for the night.
-				  	EOF
-				}	
-
-
-			]	
-		}
+		hash = {posts:Posts.all}
 		hash.to_json
 	end
+
+	put '/posts/:id' do
+		post = Posts.get(params[:id].to_i) 
+		data = JSON.parse(request.body.read)
+		result = post.update(
+			:title => data['post']['title'],
+			:intro => data['post']['intro'],
+			:description => data['post']['description']
+		)
+		'good'
+		if result 
+			['200', {'Content-Type' => 'application/json'}, [{result:true}.to_json]]
+		# else
+		# 	binding.pry
+		# 	['500', {'Content-Type' => 'application/json'}, [{result:false}.to_json]] 
+		end
+
+	end
+
 end
